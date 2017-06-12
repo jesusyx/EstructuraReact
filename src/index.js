14b753a0b8248aaf886821d1541495f40da24d0c
Jesus <jesusyx22@gmail.com>
2017-06-11 20:06:55 -0500

    Uploading

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+ReactDOM.render(<App/>,document.getElementById('root'))
